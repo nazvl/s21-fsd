7655be99ecabeb255aec23282de1b6764e01c146
vladimir <lismiroslav@yandex.ru>
2026-02-26 13:50:27 +0300

    Перенесен Footer

--- src/App/App.vue
+++ src/App/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import Loader from "@/Shared/loader/ui/loader.vue";
 import Header from "@/Widgets/Header/ui/Header.vue";
+import Footer from "@/Widgets/Footer/ui/Footer.vue";
 </script>
 
 <template>
   <Header/>
   <RouterView class="mt-14"/>
   <loader/>
+  <Footer/>
 </template>
 
 <style scoped>
---
+++ src/Widgets/Footer/ui/Footer.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { useRouter, useRoute } from 'vue-router'
+import { HomeIcon, UserIcon, CalendarIcon, AcademicCapIcon, StarIcon } from '@heroicons/vue/24/outline'
+import type { Component } from 'vue'
+import { ref, watch } from 'vue'
+
+const router = useRouter()
+const route = useRoute()
+const currentLink = ref<string | null>('')
+
+interface Link {
+  name: string
+  to: string
+  icon: Component
+}
+
+watch(
+    () => route.path,
+    (newPath) => {
+      if (newPath == '') {
+        newPath = '/'
+      }
+      currentLink.value = newPath // запись текущего маршрута в переменную при изменении
+    },
+    { immediate: true },
+)
+
+const links: Link[] = [
+  {
+    name: 'Home',
+    to: '/',
+    icon: HomeIcon,
+  },
+  {
+    name: 'Profile',
+    to: '/profile',
+    icon: UserIcon,
+  },
+  {
+    name: 'Calendar',
+    to: '/calendar',
+    icon: CalendarIcon,
+  },
+  {
+    name: 'Projects',
+    to: '/projects',
+    icon: AcademicCapIcon,
+  },
+  {
+    name: 'Find',
+    to: '/find',
+    icon: StarIcon,
+  },
+]
+
+function onNavigateTo(to: string) {
+  console.log(to)
+  router.push(to)
+}
+</script>
+
+<template>
+  <div class="fixed bottom-0 w-full h-24 bg-lightgray-900 border-t border-t-lightgray-300">
+    <ul class="flex items-center h-full text-justwhite-500 justify-around pb-8">
+      <li v-for="link in links" :key="link.name">
+        <button
+            class="flex items-center gap-[5px] justify-center flex-col w-full h-full p-2 border-b transition-colors duration-300"
+            @click="onNavigateTo(link.to)"
+            :class="{
+            'border-b-transparent': currentLink !== link.to,
+            'border-b-blue-600': currentLink === link.to,
+          }"
+        >
+          <component :is="link.icon" class="w-6 h-6" />
+          <p class="text-xs text-lightgray-300">{{ link.name }}</p>
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
