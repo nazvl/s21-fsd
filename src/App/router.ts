f1eccf16b99e6ba6d0b1d35752dcbcb6164eadfe
vladimir <lismiroslav@yandex.ru>
2026-02-26 13:57:36 +0300

    Создал Views, добавил в роутер

--- src/App/router.ts
+++ src/App/router.ts
@@ -2,12 +2,38 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/Pages/LoginView.vue'
 import {useAuthStore} from "@/Features/auth/model/loginStore.ts";
 import ProfileView from "@/Pages/ProfileView.vue";
+import HomeView from "@/Pages/HomeView.vue";
+import CalendarView from "@/Pages/CalendarView.vue";
+import FindView from "@/Pages/FindView.vue";
+import ProjectsView from "@/Pages/ProjectsView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
+            name: 'home',
+            component: HomeView,
+        },
+        {
+            path: '/calendar',
+            name: 'calendar',
+            component: CalendarView,
+        },
+        {
+            path: "/find",
+            name: 'find',
+            component: FindView
+        }
+        ,
+        {
+            path: '/projects',
+            name: 'projects',
+            component: ProjectsView,
+        }
+        ,
+        {
+            path: '/profile',
             name: 'profile',
             component: ProfileView,
         },
---
+++ src/Pages/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+CampusList
+</template>
+
+<style scoped>
+
+</style>
