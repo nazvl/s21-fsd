dada22611b834860eeea093fdbab73c9180536a0
vladimir <lismiroslav@yandex.ru>
2026-03-02 19:50:04 +0300

    Страница поиска

--- src/Entities/Profile/model/userStore.ts
+++ src/Entities/Profile/model/userStore.ts
@@ -3,6 +3,7 @@ import {ref} from "vue";
 // import {useAuthStore} from "@/Features/auth/model/loginStore.ts";
 import {fetchData} from "@/Shared/api.ts";
 import type {User} from "@/Entities/Profile/model/userTypes.ts";
+import {useLoaderStore} from "@/Shared/loader/model/loaderStore.ts";
 
 
 
@@ -10,9 +11,10 @@ export const useUserStore = defineStore("UserStore", () => {
     const data = ref<User>({} as User)
     const points = ref<Record<string, number>>({} as Record<string, number>)
     const feedback = ref<Record<string, number>>({} as Record<string, number>)
-
+    const loader = useLoaderStore()
     // const authStore = useAuthStore()
     async function getData(userName = localStorage.getItem("peerName")) {
+        loader.show()
         try {
             data.value = await fetchData(`/participants/${userName}`)
             points.value = await fetchData(`/participants/${userName}/points`)
@@ -20,6 +22,7 @@ export const useUserStore = defineStore("UserStore", () => {
         } catch (error) {
             console.error(error)
         } finally {
+            loader.hide()
         }
     }
     return {
--- src/Pages/FindView.vue
+++ src/Pages/FindView.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
 
+import CustomInput from "@/Shared/CustomInput.vue";
+import CustomButton from "@/Shared/CustomButton.vue";
+import UserProfile from "@/Entities/Profile/ui/UserProfile.vue";
+import {useUserStore} from "@/Entities/Profile/model/userStore.ts";
+import {ref} from "vue";
+
+const name = ref<string>("");
+function handleSearch() {
+  if (name.value === "" || name.value.length < 4) return null;
+  useUserStore().getData(name.value)
+}
 </script>
 
 <template>
-
+  <div class="flex p-4 gap-2">
+    <CustomInput class="w-full" v-model="name"/><CustomButton @click="handleSearch()" label="search" />
+  </div>
+<user-profile/>
 </template>
 
 <style scoped>
